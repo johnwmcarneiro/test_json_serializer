1d7bc820a733e74af31f51460b769b58b406e316
John Carneiro <johnwmcarneiro@gmail.com>
2020-06-20 12:12:56 -0300

    Update identation

--- app/controllers/endpoints_controller.rb
+++ app/controllers/endpoints_controller.rb
@@ -1,37 +1,37 @@
 class EndpointsController < ApplicationController
-	def categories
-		render json: CategorySerializer.new(Category.all)
-	end
+  def categories
+    render json: CategorySerializer.new(Category.all)
+  end
 
-	def categories_show
-		render json: CategorySerializer.new(Category.find(params[:id]))
-	end
+  def categories_show
+    render json: CategorySerializer.new(Category.find(params[:id]))
+  end
 
-	def users
-		render json: UserSerializer.new(User.all)
-	end
+  def users
+    render json: UserSerializer.new(User.all)
+  end
 
-	def posts
-		per_page = params.fetch(:per_page, 10)
-		@posts = Post.joins(:category)
-								 .includes(:category, comments: :user)
-								 .order(created_at: :desc)
-								 .limit(per_page)
+  def posts
+    per_page = params.fetch(:per_page, 10)
+    @posts = Post.joins(:category)
+                 .includes(:category, comments: :user)
+                 .order(created_at: :desc)
+                 .limit(per_page)
 
-		render json: PostSerializer.new(@posts, {
-			include: [:category, :comments, :'comments.user']
-		})
-	end
+    render json: PostSerializer.new(@posts, {
+      include: [:category, :comments, :'comments.user']
+    })
+  end
 
-	def posts_ams
-		per_page = params.fetch(:per_page, 10)
-		@posts = Post.joins(:category)
-								 .includes(:category, comments: :user)
-								 .order(created_at: :desc)
-								 .limit(per_page)
+  def posts_ams
+    per_page = params.fetch(:per_page, 10)
+    @posts = Post.joins(:category)
+                 .includes(:category, comments: :user)
+                 .order(created_at: :desc)
+                 .limit(per_page)
 
-		render json: @posts,
-					 each_serializer: PostAmsSerializer,
-					 include: 'category,comments,comments.user'
-	end
+    render json: @posts,
+           each_serializer: PostAmsSerializer,
+           include: 'category,comments,comments.user'
+  end
 end
